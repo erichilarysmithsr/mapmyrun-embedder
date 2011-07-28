601d465346e594d84446c39646ea10d6cc584fd4
jeffmcreynolds <jeffmcreynolds@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-28 13:56:38 +0000

    Adding Version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/mapmyrun-embedder/trunk@416390 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ mapmyrun.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ Plugin Name: MapMyRun Embedder
+ Plugin URI:  http://www.http://jeffmcreynolds.com/map-my-run-wordpress-plugin/
+ Description: Use Route ID and Activity Type to Embed a Map of your Activity in your Blog. Works with MapMyRun, MapMyWalk, MapMyRide and MapMyHike.
+ Author:      Jeff McReynolds
+ Version:     1.0
+ Author URI:  http://www.jeffmcreynolds.com
+
+ Copyright 2011  Jeff McReynolds  (email : jeff@jeffmcreynolds.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+ 
+ */
+
+function gen_mmr_map($atts,$content=null)
+{
+	shortcode_atts( array('id'=>'', 'type'=>''), $atts);
+	$id = $atts['id'];
+	$type = $atts['type'];
+	$mmr_code =  "<iframe id=\"mmf_blog_map\" src=\"http://js.mapmyfitness.com/embed/blogview.html?r=$id&amp;u=e&amp;t=$type\" frameborder=\"0\" width=\"400px\" height=\"500px\"></iframe>";
+    return $mmr_code;
+}
+
+add_shortcode('fitmap', 'gen_mmr_map');
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,21 @@
+=== MapMyRide Widget  ===
+Contributors: jeffmcreynolds
+Tags: workout, tracking, mapmyride, mapmyrun, mapmyfitness, mapmyhike
+Requires at least: 2.0
+Tested up to: 3.2.1
+Stable tag: 1.0
+
+This WordPress plugin works with MapMyRun, MapMyRide, MapMyWalk & MapMyHike. Add a shortag to your post with the route id and the type of activity and it will embed a map of the activity in your post.
+
+== Description ==
+
+This WordPress plugin works with MapMyRun, MapMyRide, MapMyWalk & MapMyHike. Add a shortag to your post with the route id and the type of activity and it will embed a map of the activity in your post.
+
+== Installation ==
+
+1. Upload files to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Use the shortag to embed maps, specifying the route id(id) and the activity type(type)
+
+== Frequently Asked Questions ==
+
